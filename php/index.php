ad9a8dd9855233b0e686f05be1bc76a3ba9c763f
alimortada718 <79694567+alimortada718@users.noreply.github.com>
2021-03-09 15:35:45 +0200

    php

---
+++ php/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<div style="color:green;">
+<?php
+echo "hello its me ali";
+?>
+</div>
+<div style="color:green;">
+<?php
+echo "hello its me ali";
+?>
+</div>
+<div style="border:1px solid black;">
+<?php
+echo "hello its me red";
+?>
+
+<div style="color:green;">
+<?php
+echo "hello its me ali";
+?>
+</div>
+
+</div>
+
+
+</body>
+
+</html>
