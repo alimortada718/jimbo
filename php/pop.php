220735edf9e681f23676632ee9bd01ff0009e3ef
alimortada718 <79694567+alimortada718@users.noreply.github.com>
2021-03-19 01:04:23 +0200

    uganda

---
+++ php/pop.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+
+
+<div style="color:violet;">
+<?php
+echo "the king has arrived";
+?>
+</div>
+<div style="color:green;">
+<?php
+echo "hello its ya boy ali";
+?>
+</div>
+<div style="border:1px solid black;">
+<?php
+?>
+</html>
+ 
