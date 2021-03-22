eae249b456b5ad6061615618c5897455d6e1dbb6
alimortada718 <79694567+alimortada718@users.noreply.github.com>
2021-03-22 19:28:05 +0200

    php

---
+++ php/bmw.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<div style="color:blue;">
+<?php
+echo "hello its me ali";
+?>
+</div>
+<div style="color:red;">
+<?php
+echo "amen";
+?>
+</div>
+<div style="border:4px solid black;">
+<?php
+echo "hello its ya boy";
+?>
+
+<div style="color:yellow;">
+<?php
+echo "hello its me ali718";
+?>
+</div>
+
+</div>
+
+
+</body>
+
+</html>
