78fdccfd6f68aa5a2433adfd19231d4da30d926e
alimortada718 <79694567+alimortada718@users.noreply.github.com>
2021-03-24 17:38:42 -0400

    php

---
+++ php/hyper.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <divstyle="<color:orange;">
+ <?php 
+ echo"welcometo ma page;"
+ ?>
+</div>
+<div style="color:red;">
+<?php
+echo "qack";
+?>
+</div>
+<div style="border:420px solid black;">
+<?php
+echo "hello its ya boy";
+?>
+
+<div style="color:green;">
+<?php
+echo "hello its me ali718";
+?>
+</div>
+
+</div>
+
+
+</body>
+
+</html>
+          
