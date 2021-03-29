ffea4c842f528c5d714a3e28c2a8b07a580cd3e1
alimortada718 <79694567+alimortada718@users.noreply.github.com>
2021-03-29 13:36:29 -0400

    php

---
+++ php/numb.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <divstyle="<color:orange;">
+ <?php 
+ echo"welcome to ma page;"
+ ?>
+</div>
+<div style="color:violet;">
+<?php
+echo "wack";
+?>
+</div>
+<div style="border:420px solid blue;">
+<?php
+echo "hello its ya boy";
+?>
+
+<div style="color:green;">
+<?php
+echo "hello its ya boy ";
+?>
+</div>
+
+</div>
+
+
+</body>
+
+</html>
