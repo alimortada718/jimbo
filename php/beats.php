b7caf315ab5312c79e1724988d0b4dfd5847cfc9
alimortada718 <79694567+alimortada718@users.noreply.github.com>
2021-03-31 06:37:19 -0400

    php

---
+++ php/beats.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <div style ="color:green;" >
+        <?php
+        echo"brrrr";
+        ?>
+        </div>
+        <div style="color:cyan;">
+    <?php
+    echo"money man$";
+    ?>
+    </div>
+    <div style = "border: 2px solid black;">
+    <?php
+    ?>
+    </html>
